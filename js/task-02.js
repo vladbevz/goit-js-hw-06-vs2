848163d291a3955b99c59aead65cd6ec224b67a7
vladbevz <bevz.vlad15@gmail.com>
2024-06-09 15:31:52 +0200

    update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsList = document.querySelector("#ingredients");
+
+const listEl = document.createElement("li");
+
+ingredients.forEach((ingredient) => {
+  const listEl = document.createElement("li");
+  listEl.textContent = ingredient;
+  listEl.classList.add("item");
+  ingredientsList.append(listEl);
+  console.log(ingredientsList);
+});
