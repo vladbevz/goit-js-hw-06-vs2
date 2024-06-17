e1b08787e5aa86235cc3d873874caca391268b5f
vladbevz <bevz.vlad15@gmail.com>
2024-06-17 21:03:57 +0200

    update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+const backgroundSpace = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", onchangeColorBtn);
+
+function onchangeColorBtn() {
+  const randomColor = getRandomHexColor();
+  backgroundSpace.style.backgroundColor = randomColor;
+  colorName.textContent = randomColor;
+}
